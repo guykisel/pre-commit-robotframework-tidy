c71ea81d3bb42c2664824b21bae67cea1eb02239
gkisel <gkisel@truecar.com>
2014-10-13 15:59:20 -0700

    Fix typo in entry point name

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     entry_points={
         'console_scripts': [
             ('python-robotframework-tidy = '
-             'pre_commit_robotframework_tidy.rf_tify:main'),
+             'pre_commit_robotframework_tidy.rf_tidy:main'),
         ],
     },
 )
